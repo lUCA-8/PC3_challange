ed687f1faf31733518852eb85ffb1cf6b95a9c12
Luca <eslam.m.mostafa@outlook.com>
2021-07-07 14:54:30 +0200

    Final upload

---
+++ index.js
@@ -0,0 +1,73 @@
+function togglePopup() {
+  document.getElementById("popup_1").classList.toggle("active");
+}
+
+//Mouse Over:
+document.getElementById("icon_1").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Karrierewage";
+  document.getElementById("text_description_paragraph").innerHTML =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_2").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Habilitation";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_3").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Berfungs-verfahren";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_4").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Familie, Partnerschaft, Alltag";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_5").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Blick aus dem Ausland";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_6").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Promotion";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_7").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Juniorprofessur";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_8").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Arbeitsplatz";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_9").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Mobilität";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+document.getElementById("icon_10").addEventListener("mouseover", function () {
+  document.getElementById("text_description_header").innerHTML = "Allgemein";
+  document.getElementById("text_description_paragraph").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+});
+
+//Mouse Out:
+
+document.getElementById("icon_1").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_2").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_3").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_4").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_5").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_6").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_7").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_8").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_9").addEventListener("mouseout", mouseOut);
+document.getElementById("icon_10").addEventListener("mouseout", mouseOut);
+
+function mouseOut() {
+  document.getElementById("text_description_header").innerHTML = "";
+  document.getElementById("text_description_paragraph").innerHTML = "";
+}
